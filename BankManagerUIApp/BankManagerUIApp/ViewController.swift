de61775e5c1575aa49c4d652a06b359ddbf809ac
웡빙 <wb277@naver.com>
2022-07-07 18:11:07 +0900

    test :UI 구성

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -7,12 +7,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var mainView: MainView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainView = MainView(self)
+        mainView?.addTenCustomerButton.addTarget(self, action: #selector(addTenCustomerButtonDidTapped), for: .touchUpInside)
+        mainView?.resetButton.addTarget(self, action: #selector(resetButtonDidTapped), for: .touchUpInside)
     }
-
-
+    
+    @objc func addTenCustomerButtonDidTapped(_ sender: UIButton) {
+        print("10명의 고객 추가")
+    }
+    
+    @objc func resetButtonDidTapped(_ sender: UIButton) {
+        print("리셋버튼 ")
+    }
 }
 
